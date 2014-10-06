5a764ad2f87ab55dfb2412924abbb1ac10b2b0cd
Paul Tagliamonte <tag@pault.ag>
2014-10-05 23:30:02 -0400

    foo

--- travel/admin.py
+++ travel/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Trip, ServiceProvider, Flight
+
+
+for klass in [Trip, ServiceProvider, Flight]:
+    class DefaultAdmin(admin.ModelAdmin):
+        pass
+    admin.site.register(klass, DefaultAdmin)
--- travel/models.py
+++ travel/models.py
@@ -13,10 +13,14 @@ class ServiceProvider(models.Model):
     website = models.TextField()
     phone_number = models.TextField()
 
+    def __str__(self):
+        return "<ServiceProvider: {}>".format(self.name)
+
 
 class Flight(models.Model):
     user = models.ForeignKey(User)
     departure_time = models.DateTimeField()
+    flight_number = models.TextField()
     arrival_time = models.DateTimeField()
     carrier = models.ForeignKey(ServiceProvider)
     trip = models.ForeignKey(Trip, related_name="flights")
