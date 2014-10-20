90457f3edb58b6bc90fab97a3f4ee19022677da6
Paul Tagliamonte <tag@pault.ag>
2014-10-20 19:31:47 -0400

    add dj static

--- public/settings.py
+++ public/settings.py
@@ -48,4 +48,5 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 ENABLE_MAPBOX = os.environ.get("TRAVEL_ENABLE_MAPBOX", "True").lower() == "true"
--- public/wsgi.py
+++ public/wsgi.py
@@ -1,4 +1,5 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "public.settings")
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling, MediaCling
+application = Cling(MediaCling(get_wsgi_application()))
